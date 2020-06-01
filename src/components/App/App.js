ef3320ed6dac81d1014e74470d5fd561c81ca6b3
koZIcka2019 <lukasz.kopyto@gmail.com>
2020-06-01 20:44:40 +0200

    basic configuration of the project and creation of its structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Test</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
