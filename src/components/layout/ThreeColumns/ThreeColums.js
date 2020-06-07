248690552d11b23f6f9821f0409fcb7ae6f0e309
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-06-07 08:21:56 +0200

    adding a threeColumns section

--- src/components/layout/ThreeColumns/ThreeColums.js
+++ src/components/layout/ThreeColumns/ThreeColums.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Background from '../../../assets/3ColumnsBackground.png'
 
 const ThreeColums = () => {
   return (
-    <div className='threeColumns' name='threeColums'>
-      <h1>ThreeColumns</h1>
-    </div>
+    <section
+      className='threeColumns'
+      name='threeColums'
+      style={{ backgroundImage: `url(${Background})` }}
+    >
+      <div className='threeColumns__box'>
+        <span>10</span>
+        <h2>oddanych worków</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </div>
+      <div className='threeColumns__box'>
+        <span>5</span>
+        <h2>wspartych organizacji</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </div>
+      <div className='threeColumns__box'>
+        <span>7</span>
+        <h2>zorganizowanych zbiórek</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </div>
+    </section>
   )
 }
 
