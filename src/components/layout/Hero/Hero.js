ce6f844d8dcbd5ab8d5e0b8a80dd555af02434a3
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-06-07 12:36:53 +0200

    fix buttons in hero section

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -16,8 +16,12 @@ const Hero = () => {
         </h1>
         <img src={decoration} alt='' />
         <div className='hero__links'>
-          <Link to='/oddaj-rzecz'>ODDAJ RZECZY</Link>
-          <Link to='/oddaj-rzecz'>ZORGANIZUJ ZBIÓRKĘ</Link>
+          <Link to='/oddaj-rzecz' className='btn-primary'>
+            ODDAJ RZECZY
+          </Link>
+          <Link to='/oddaj-rzecz' className='btn-primary'>
+            ZORGANIZUJ ZBIÓRKĘ
+          </Link>
         </div>
       </div>
     </section>
