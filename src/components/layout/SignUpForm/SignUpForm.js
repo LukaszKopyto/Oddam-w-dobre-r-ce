3d3c7e7b885cc3a9af3912d7556d5eed7be10f36
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-06-11 08:51:09 +0200

    style SignUpPage view

--- src/components/layout/SignUpForm/SignUpForm.js
+++ src/components/layout/SignUpForm/SignUpForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 
 const INITIAL_STATE = {
   username: '',
@@ -54,38 +54,43 @@ const SignUpForm = ({ firebase }) => {
 
   return (
     <form onSubmit={onSubmit}>
-      <input
-        name='username'
-        value={username}
-        onChange={onChange}
-        type='text'
-        placeholder='Podaj Imię'
-      />
-      <input
-        name='email'
-        value={email}
-        onChange={onChange}
-        type='text'
-        placeholder='Adres e-mail'
-      />
-      <input
-        name='passwordOne'
-        value={passwordOne}
-        onChange={onChange}
-        type='password'
-        placeholder='Hasło'
-      />
-      <input
-        name='passwordTwo'
-        value={passwordTwo}
-        onChange={onChange}
-        type='password'
-        placeholder='Potwierdz hasło'
-      />
-      <button disabled={isInvalid} type='submit'>
+      <div className='form-wrapper'>
+        <label htmlFor='username'>Podaj Imię</label>
+        <input
+          name='username'
+          value={username}
+          onChange={onChange}
+          type='text'
+        />
+        <label htmlFor='email'>Adres e-mail</label>
+        <input name='email' value={email} onChange={onChange} type='text' />
+        <label htmlFor='passwordOne'>Hasło</label>
+        <input
+          name='passwordOne'
+          value={passwordOne}
+          onChange={onChange}
+          type='password'
+        />
+        <label htmlFor='passwordTwo'>Potwierdź hasło</label>
+        <input
+          name='passwordTwo'
+          value={passwordTwo}
+          onChange={onChange}
+          type='password'
+        />
+      </div>
+      <button
+        disabled={isInvalid}
+        type='submit'
+        className='btn-small'
+        title='Załóż konto'
+      >
         Załóż konto
       </button>
       {error && <p>{error.message}</p>}
+      <Link to='/logowanie' title='Zaloguj się'>
+        Zaloguj się
+      </Link>
     </form>
   )
 }
--- src/components/views/SignUpPage/SignUpPage.js
+++ src/components/views/SignUpPage/SignUpPage.js
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react'
 import SignUpForm from '../../layout/SignUpForm'
 import { FirebaseContext } from '../../Firebase'
+import HomeHeader from '../../layout/HomeHeader'
+import decoration from '../../../assets/Decoration.svg'
 
 const SignUpPage = () => {
   const firebase = useContext(FirebaseContext)
   return (
-    <div>
-      <h1>Załóż konto</h1>
-      <SignUpForm firebase={firebase} />
-    </div>
+    <>
+      <HomeHeader />
+      <div className='SignUpPage'>
+        <h1>Załóż konto</h1>
+        <img src={decoration} alt='' className='decoration' />
+        <SignUpForm firebase={firebase} />
+      </div>
+    </>
   )
 }
 
