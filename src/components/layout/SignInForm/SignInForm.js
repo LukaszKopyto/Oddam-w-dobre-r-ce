4eb29790d99a9c2dd272be02b9b93bcd39141e14
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-06-12 10:27:16 +0200

    adding validation to SignIn Form

--- src/components/layout/SignInForm/SignInForm.js
+++ src/components/layout/SignInForm/SignInForm.js
@@ -25,15 +25,46 @@ const SignInForm = ({ firebase }) => {
       })
   }
 
+  const validate = (values) => {
+    let errors = {}
+
+    const { email, password } = values
+
+    if (!email) {
+      errors.email = 'Pole wymagane'
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Niepoprawny adres email'
+    }
+
+    if (!password) {
+      errors.password = 'Pole wymagane'
+    } else if (password.length < 6) {
+      errors.password = 'Hasło musi zawierać conajmniej 6 znaków'
+    }
+    return errors
+  }
+
   return (
     <>
-      <Formik initialValues={INITIAL_STATE} onSubmit={onSubmit}>
+      <Formik
+        initialValues={INITIAL_STATE}
+        onSubmit={onSubmit}
+        validate={validate}
+      >
         <Form>
           <div className='form-wrapper'>
             <label htmlFor='email'>E-mail</label>
             <Field name='email' type='text' />
+            <ErrorMessage name='email'>
+              {(msg) => <span className='error__info'>{msg}</span>}
+            </ErrorMessage>
             <label htmlFor='password'>Hasło</label>
             <Field name='password' type='password' />
+            <ErrorMessage name='password'>
+              {(msg) => <span className='error__info'>{msg}</span>}
+            </ErrorMessage>
           </div>
           <button type='submit' className='btn-small' title='Zaloguj się'>
             Zaloguj się
