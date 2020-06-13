42f9ff162a89defcd09f8d3bf1b5990716679dac
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-06-13 13:32:08 +0200

    adding style do StepsButtons

--- src/components/layout/HandOverForm/StepsButtons.js
+++ src/components/layout/HandOverForm/StepsButtons.js
@@ -4,16 +4,31 @@ const StepsButtons = ({ prev, next, submit, step }) => {
   return (
     <>
       {step !== 1 && (
-        <button type='button' onClick={prev}>
+        <button
+          type='button'
+          onClick={prev}
+          className='btn-small'
+          title='Wstecz'
+        >
           Wstecz
         </button>
       )}
       {step === 5 ? (
-        <button type='button' onClick={submit}>
+        <button
+          type='button'
+          onClick={submit}
+          className='btn-small'
+          title='Potwierdzam'
+        >
           Potwierdzam
         </button>
       ) : (
-        <button type='button' onClick={next}>
+        <button
+          type='button'
+          onClick={next}
+          className='btn-small'
+          title='Dalej'
+        >
           Dalej
         </button>
       )}
